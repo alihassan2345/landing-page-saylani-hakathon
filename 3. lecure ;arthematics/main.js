3ebcb92731309e8989b5871c19cb4baa1f614255
alihassan2345 <160609826+alihassan2345@users.noreply.github.com>
2025-01-17 22:34:45 +0500

    bb

---
+++ 3. lecure ;arthematics/main.js
@@ -0,0 +1,34 @@
+//addition(+)
+var num1 = 12;
+var num2 = 16;
+console.log(num1 + num2);
+//subtraction(-)
+var num3 = 60;
+var num4 = 50;
+console.log(num3 - num4);
+//multiplication(*)
+var num5 = 7;
+var num6 = 9;
+console.log(num5 * num6);
+//division(/)
+var num7 = 16;
+var num8 = 4;
+console.log(num7 / num8);
+//modulus(%)
+var num9 = 14;
+var num10 = 18;
+console.log(num9 % num10);
+//exponentional(**)
+var num11 = 5;
+var num12 = 5;
+console.log(Math.pow(num11, num12));
+//increment(++)
+var b = 12;
+console.log(b++);
+console.log(b);
+console.log(b++);
+//decreament(--)
+var d = 14;
+console.log(d--);
+console.log(d);
+console.log(d--);
---
+++ 3. lecure ;arthematics/main.ts
@@ -0,0 +1,34 @@
+//addition(+)
+let num1 :number=12
+let num2 :number=16
+console.log(num1 + num2);
+//subtraction(-)
+let num3 :number=60
+let num4 :number=50
+console.log(num3 - num4);
+//multiplication(*)
+let num5 :number=7
+let num6 :number=9
+console.log(num5 * num6);
+//division(/)
+let num7 :number=16
+let num8 :number=4
+console.log(num7 / num8);
+//modulus(%)
+let num9 :number=14
+let num10 :number=18
+console.log(num9 % num10);
+//exponentional(**)
+let num11 :number=5
+let num12 :number=5
+console.log(num11 ** num12);
+//increment(++)
+let b=12
+console.log(b++)
+console.log(b)
+console.log(b++)
+//decreament(--)
+let d=14
+console.log(d--)
+console.log(d)
+console.log(d--)
